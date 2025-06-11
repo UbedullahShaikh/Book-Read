7a434ce0356955076c154ac5057d5cd088c3a1e2
UbedullahShaikh <shykhubedullah@gmail.com>
2025-06-12 02:15:18 +0500

    Test tailwind connectivity

--- Smart Book Reading Web App UI/src/App.jsx
+++ Smart Book Reading Web App UI/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className='bg-red-500 w-full h-full flex justify-center items-center'>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -16,8 +16,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
+      <h1 className='text-3xl font-bold underline'>Vite + React</h1>
+      <div className="bg-blue-500 w-full h-full flex justify-center items-center">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -25,7 +25,7 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className="text-3xl font-bold underline">
         Click on the Vite and React logos to learn more
       </p>
     </>
